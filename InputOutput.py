ecb996602aba374d62b5956f5b6c95e0f92155ac
sajansinghthakuri <237443145+sajansinghthakuri@users.noreply.github.com>
2026-09-11 21:34:44 +0545

    Input, Output and Data Type Conversion

---
+++ InputOutput.py
@@ -0,0 +1,8 @@
+name = input("What is your name?")
+print("Hello", name)
+
+# String converted to int
+age = int(input("How old are you?"))
+print(
+    name, "you are", age, "years old.", "You will be", age + 1, "years old next year."
+)
