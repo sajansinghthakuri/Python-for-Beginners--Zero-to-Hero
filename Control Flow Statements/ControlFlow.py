772bb9f9ae5cc44668c40c0388b0d9aca5215aee
sajansinghthakuri <237443145+sajansinghthakuri@users.noreply.github.com>
2026-09-17 22:37:40 +0545

    Conditional Statements (if,elif,else)

---
+++ Control Flow Statements/ControlFlow.py
@@ -0,0 +1,11 @@
+# if, elif, else statements are used to control the flow of a program based on certain conditions.
+
+# if condition1:
+# code block to execute if condition1 is True
+x = int(input("Enter a number:"))
+if x > 0:
+    print(x, "is positive")
+elif x == 0:
+    print("Number is zero")
+else:
+    print(x, "is not positive")
